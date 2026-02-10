bf0a4d9193d6ae9186e900220a7cb2939a862bdc
Simon Zheng <simonw713jb@gmail.com>
2026-02-10 17:02:47 +0800

    Add refresh_rank variable in struct blk_info
    This commit introduces a new field to the block structure to record the rank of
    each block within the Multiple-Rank Queue.
    
    The new field added to the Block structure is:
    - int refresh_rank;

--- initialize.h
+++ initialize.h
@@ -287,6 +287,7 @@ struct blk_info{
     unsigned int free_page_num;        //记录该块中的free页个数，同上
     unsigned int invalid_page_num;     //记录该块中失效页的个数，同上
     int last_write_page;               //记录最近一次写操作执行的页数,-1表示该块没有一页被写过
+    int refresh_rank;                  //記錄塊在 Multiple-rank queue 中的 rank
     struct page_info *page_head;       //记录每一子页的状态
 };
 
